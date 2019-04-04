2a90c2eba4a5bbd3112561e774260d9b8f67b598
AntonBrito <tonybrito85@gmail.com>
2019-04-04 00:43:56 -0400

    Fix some typo

--- models/User.js
+++ models/User.js
@@ -16,12 +16,11 @@ const UserSchema = new Schema({
     required: true
   },
   avatar: {
-    type: String,
-    required: true
+    type: String
   },
   date: {
     type: Date,
-    default: date.now
+    default: Date.now
   }
 });
 
