64f8b85f3acdf94bcb30cda4be9e07ad5799ce4e
AntonBrito <tonybrito85@gmail.com>
2019-04-18 11:41:09 -0400

    Fixed a typo

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,11 @@
-const JWTStrategy = require("passport-jwt").Strategy;
-const ExtractJWT = require("passport-jwt").ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("../config/keys");
 
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeadeAsBearerToken();
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
