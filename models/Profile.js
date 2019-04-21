27b629e50bfe54461ba6956ca523e5f712c83d97
AntonBrito <tonybrito85@gmail.com>
2019-04-21 00:35:05 -0400

    Created profile model

---
+++ models/Profile.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  username: {
+    type: String
+  },
+  contact: {
+    type: String
+  }
+});
+
+module.exports = Profile = mongoose.model("Profile", ProfileSchema);
